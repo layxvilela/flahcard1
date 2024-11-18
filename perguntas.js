f30acb8518eeba261cf088aac6053468bf853191
layarinha <142330762+layxvilela@users.noreply.github.com>
2024-11-18 12:02:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Chanel',
+    'O que é a Chanel?',
+    'Uma marca de luxo, mundialmente conhecida'
+)
+
+
+criaCartao(
+    'Chanel',
+    'Em qual ano a marca foi criada?',
+    'Foi criada em 1924'
+)
+
+
+criaCartao(
+    'Chanel',
+    'Quem criou a marca?',
+    'Gabrielle Chanel,mais conhecida como "Coco Chanel"'
+)
+
+
+criaCartao(
+    'Chanel',
+    'Quais produtos a marca fabrica?',
+    'Alta Costura, Moda, Alta Joalheria, Joalheria, Relógios, Óculos e maquiagem'
+)
+
+criaCartao(
+    'Chanel',
+    'Qual o produto mais vendido?',
+    ' Perfumes, entre eles Chanel N°5, Coco mademoiselle e gabrielle'
+)
+
+
+criaCartao(
+    'Chanel',
+    'Chanel ocupa qual posição no ranking de marcas famosas?',
+    'Atualmente ela ocupa o 4° lugar'
+)
+
+
+criaCartao(
+    'Chanel',
+    'Onde fica a sede da chanel?',
+    'Londres, Reino Unido'
+)
+
+
+criaCartao(
+    'Chanel',
+    'Quantas boutiques a marca possui?',
+    'Em todo o mundo, a Chanel S.A. opera cerca de 310 butiques'
+)
+
